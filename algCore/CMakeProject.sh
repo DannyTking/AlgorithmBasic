7ff6cfb18ad3d6571e020ce0820f109c1e28723e
gytang@HW_Node <edwardtang@yeah.net>
2021-05-15 18:14:27 +0800

    add CMakeProject.sh

---
+++ algCore/CMakeProject.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p src
+touch src/README.md
+
+mkdir -p input
+touch input/README.md
+
+mkdir -p config
+touch input/README.md
+
+mkdir -p output
+touch input/README.md
+
+[ ! -f "CMakeLists.txt" ] && > CMakeLists.txt
+
+[ ! -f "build.sh" ] && > build.sh
+
+[ ! -f "run.sh" ] && > run.sh
+
+cd src
+
+touch std_inc.h
+touch io_inc.h
+touch open_3rd_inc.h
+touch main.cpp
+touch CMakeLists.txt
