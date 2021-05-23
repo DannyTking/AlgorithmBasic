7059abbc34e970703322bcb0e2a7a9d85396875b
gytang@HW_Node <edwardtang@yeah.net>
2021-05-23 11:40:29 +0800

    add routes SymbolGrap

--- algCore/run.sh
+++ algCore/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 hms=`date +%H:%M:%S`
 out=./output/run_${hms}.log
-cat ./input/paths.txt | ./build/algCore  > ${out}
+./build/algCore  > ${out}
 cat ${out}
---
+++ algCore/src/SymbolGraph.cpp
@@ -0,0 +1,1 @@
+#include "SymbolGraph.h" 
